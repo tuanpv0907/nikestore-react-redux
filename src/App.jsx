aba20a020cf408dd47dbf2453fe13087c107352c
Tuan Pham <tuanpv0907@gmail.com>
2022-10-13 11:02:50 +0700

    up new project

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Cart, FlexContent, Footer, Hero, Navbar, Sales, Stories } from "./components";
+import {
+  heroapi,
+  popularsales,
+  toprateslaes,
+  highlight,
+  sneaker,
+  story,
+  footerAPI,
+} from "./data/data.js";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Cart />
+      <main className="flex flex-col gap-16 relative">
+        <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
+
+        <Sales endpoint={toprateslaes} />
+        <FlexContent endpoint={sneaker} />
+        <Stories story={story} />
+        <Footer footerAPI={footerAPI} />
+      </main>
+    </>
+  );
+};
+
+export default App;
